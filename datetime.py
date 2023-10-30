205dab35b2a35372df95821506a42ed8738921c3
Prabuddha Sonalkar <132130570+Prabuddha-Sonalkar@users.noreply.github.com>
2023-10-29 23:20:15 -0700

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,5 +1,5 @@
 import os
 import datetime as dt
-print("last accessed time",os.path.getatime("E:/Aditi"))
-print("last modified time",os.path.getmtime("E:/Aditi"))
-# access_time=dt.timestamp(os.path.getatime("E:/Aditi"))
+print("last accessed time",os.path.getatime("E:/TY_!10"))
+print("last modified time",os.path.getmtime("E:/Ty_!0"))
+# access_time=dt.timestamp(os.path.getatime("E:/TY_10"))
